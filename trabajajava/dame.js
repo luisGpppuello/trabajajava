41ef48e9fc5fb13cf40029cfdbe63840a7dbba6f
LUIS GUSTAVO PUELLO PUERTA <gpuello902@gmail.com>
2022-03-04 19:59:48 -0500

    se sube el documento

---
+++ trabajajava/dame.js
@@ -0,0 +1,57 @@
+/*ejemplo de imprimir por consola 
+const btn = document.getElementById("add");
+btn.addEventListener("click", function (){
+   console.log("click"); 
+})*/
+document.addEventListener("DOMContentLoaded",function(){
+    const title = document.getElementById("title");
+    const description = document.getElementById("description");
+    const table = document.getElementById("table");
+    const alert = document.getElementById("alert");
+    const btn = document.getElementById("add");
+
+    /*esta funcion me muestra por consola agrevar datos o error
+    function addtodo (){
+        if (title.value === "" || description.value === ""){
+            console.error("title and description are requeried");
+        }else{
+            console.log("ok");
+        }
+    }*/
+    function addtodo (){
+        if (title.value === "" || description.value === ""){
+            alert.classList.remove("d-none");
+            alert.innerText = "title and description are requeried";
+            return;
+        }
+        alert.classList.add("d-none");
+        const row = table.insertRow();
+        row.innerHTML = `
+        <td> ${title.value} </td>
+        <td> ${description.value} </td>
+        </td>
+           <td class="text-center">
+             <input type="checkbox">
+         </td>
+          <td class="text-right">
+             <button class="btn btn-primary mb-1">
+               <i class="fa fa-pencil"></i>
+             </button>
+               <button class="btn btn-danger mb-1 ml-1">
+                <i class="fa fa-trash"></i>
+             </button>
+        </td>
+
+        `;
+
+    }
+    btn.onclick = addtodo;
+})
+
+
+
+
+
+
+
+
